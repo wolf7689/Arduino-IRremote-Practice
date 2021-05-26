fd91fbfea854f8962fb4901688c496c1375b877b
wolf7689 <michaelwolf.wcs@gmail.com>
2021-05-27 01:24:46 +0800

    fix structure

--- Arduino_IRremote_Practice/Arduino_IRremote_Practice.ino
+++ Arduino_IRremote_Practice/Arduino_IRremote_Practice.ino
@@ -45,10 +45,31 @@ void setup()
  * and a variable 8 bit command.
  * There are exceptions like Sony and Denon, which have 5 bit address.
  */
-uint16_t sAddress = 0x0102;
-uint8_t sCommand = 0x34;
-uint8_t sRepeats = 0;
+uint16_t sAddress = 0x10;
+//电源
+uint8_t sCommand1 = 0x5D;
+//上
+uint8_t sCommand2 = 0x45;
+//下
+uint8_t sCommand3 = 0x55;
+//左
+uint8_t sCommand4 = 0x7;
+//右
+uint8_t sCommand5 = 0x1;
+//ok
+uint8_t sCommand6 = 0x41;
+//返回
+uint8_t sCommand7 = 0x0;
+uint8_t sRepeats = 3;
 int inByte = 0;         // incoming serial byte
+//输入
+unsigned int buf1[67] = { 4450,4450,550,1650,550,1650,550,1700,500,600,550,550,550,550,550,550,550,550,550,1700,500,1700,550,1650,550,550,550,550,550,600,550,550,550,550,550,1650,550,550,550,550,550,600,500,600,550,550,550,550,550,550,550,550,550,1700,500,1700,550,1650,550,1650,550,1700,500,1700,550,1650,550 };
+//电源
+unsigned int buf2[67] = { 4450,4500,500,1700,550,1650,550,1700,500,600,550,550,550,550,550,550,550,600,500,1700,550,1650,550,1700,500,600,550,550,550,550,550,550,550,550,550,600,500,1700,550,550,550,550,550,600,500,600,550,550,550,550,550,1650,550,600,500,1700,550,1650,550,1700,500,1700,550,1650,550,1700,550 };
+//音量+
+unsigned int buf3[67] = { 4450,4450,550,1650,600,1650,550,1650,550,550,550,550,550,550,600,500,600,550,550,1650,550,1650,600,1600,600,550,550,550,550,550,550,550,550,550,600,1650,550,1650,550,1650,600,500,600,500,600,550,550,550,550,550,550,550,600,500,600,500,600,1650,550,1650,550,1650,600,1650,550,1650,550 };
+//音量-
+unsigned int buf4[67] = { 4450,4450,550,1650,600,1600,600,1650,550,550,550,550,550,550,600,500,600,550,550,1650,550,1650,550,1650,600,550,550,550,550,550,550,550,550,550,550,1650,600,1650,550,550,550,1650,550,550,600,500,600,550,550,550,550,550,550,550,550,1650,600,500,600,1650,550,1650,550,1650,600,1650,550 };
 
 void loop() 
 {
@@ -62,7 +83,7 @@ void loop()
         * Print current send values
         */
         Serial.println(inByte);
-        Serial.print(F("Send now: address=0x"));
+        /*Serial.print(F("Send now: address=0x"));
         Serial.print(sAddress, HEX);
         Serial.print(F(" command=0x"));
         Serial.print(sCommand, HEX);
@@ -71,10 +92,11 @@ void loop()
         Serial.println();
 
         Serial.println(F("Send NEC with 16 bit address"));
-        Serial.flush();
+        Serial.flush();*/
 
         // Results for the first loop to: Protocol=NEC Address=0x102 Command=0x34 Raw-Data=0xCB340102 (32 bits)
-        IrSender.sendNEC(sAddress, sCommand, sRepeats);
+        IrSender.sendNEC(sAddress, sCommand1, sRepeats);
+        //IrSender.sendRaw(buf2, 67, 38);
 
         /*
          * If you cannot avoid to send a raw value directly like e.g. 0xCB340102 you must use sendNECRaw()
@@ -85,13 +107,13 @@ void loop()
               * Increment send values
               * Also increment address just for demonstration, which normally makes no sense
               */
-        sAddress += 0x0101;
-        sCommand += 0x11;
-        sRepeats++;
-        // clip repeats at 4
-        if (sRepeats > 4) {
-            sRepeats = 4;
-        }
+        //sAddress += 0x0101;
+        //sCommand += 0x11;
+        //sRepeats++;
+        //// clip repeats at 4
+        //if (sRepeats > 4) {
+        //    sRepeats = 4;
+        //}
 
         delay(1000);  // delay must be greater than 5 ms (RECORD_GAP_MICROS), otherwise the receiver sees it as one long signal
     }
